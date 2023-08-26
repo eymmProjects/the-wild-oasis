f6ab6f349aa0f97c677705fb7a17e0f77cff945d
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-08-26 23:06:46 +0800

    initialize supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,21 @@
 import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://wbyvutaafajoeaxqpqgs.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-08-26T08%3A38%3A33.477Z"
+        alt=""
+      />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wbyvutaafajoeaxqpqgs.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndieXZ1dGFhZmFqb2VheHFwcWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMwMzEyNDcsImV4cCI6MjAwODYwNzI0N30.Je1uNj9HXrxVG6dhkHKLAAVwqzC0PUse4ro6hH7prjg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
